42437f48cc56c6caf32885eb7d869b93e6045304
gunasukesh-942 <141554542+gunasukesh-942@users.noreply.github.com>
2024-02-12 12:06:18 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,3 +1,3 @@
 def call (
-  git branch: 'masterr', credentialsId: '913299d4-9ccf-4917-832b-32c1482fad73', url: 'https://github.com/gunasukesh-942/one.git'
+  git branch: 'master', credentialsId: '913299d4-9ccf-4917-832b-32c1482fad73', url: 'https://github.com/gunasukesh-942/one.git'
 )
