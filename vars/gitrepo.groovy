52244963091c3baa1b46080de35a0cbedbafd375
gunasukesh-942 <141554542+gunasukesh-942@users.noreply.github.com>
2024-02-12 12:28:52 +0530

    Update gitrepo.groovy

--- vars/gitrepo.groovy
+++ vars/gitrepo.groovy
@@ -1,4 +1,4 @@
 def call(String Branch,String repo) {
   git branch: "${Branch}", credentialsId: '913299d4-9ccf-4917-832b-32c1482fad73', url: '${repo}'
-  sh "mvn clean package"
+  sh 'echo "successfully vloned git repo"'
 }
